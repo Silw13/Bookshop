e7c1f3e1926d59a3666a25282c2dfd8a586bed0b
Куницкий Ростислав Борисович <kunitskiy.rb@cso.com>
2023-05-04 15:01:27 +0300

    img copy

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
                 { from: 'src/img', to: 'img' },
                 { from: 'src/icons', to: 'icons' }
             ],
-            
+
         })
     ],
     module: {
