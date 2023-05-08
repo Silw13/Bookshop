594a0197550b267411219939f209fd0e0d716245
Куницкий Ростислав Борисович <kunitskiy.rb@cso.com>
2023-05-08 11:46:30 +0300

    base css

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js'
+        filename: 'js/bundle.js'
     },
     mode: 'production',
     plugins: [
